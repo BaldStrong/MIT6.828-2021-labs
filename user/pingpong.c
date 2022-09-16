a09d3219e5e0a4a0f23aff07b15878947d55fda6
BaldStrong <BaldStrong@qq.com>
2022-09-16 20:34:59 +0800

    no close pipe

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,32 +4,42 @@
 int
 main(int argc, char* argv[]) {
   int p[2];
-  int pp[2];
+  // int pp[2];
   char buf[1];
   pipe(p); //父进程写，子进程读
-  pipe(pp);
+  // pipe(pp);
   /**
    * @brief Construct a new if object
    * 父进程先write，然后子进程read（等待write），printf，
    * 然后子进程再write，父进程收到后read，printf
    * 
    */
+  // if (fork() == 0) {
+  //   // close(p[1]);
+  //   read(p[0], buf, sizeof(buf));
+  //   printf("%d: received ping\n",getpid());
+  //   write(p[1], " ", 1);
+  //   close(p[0]);
+  // }  else {
+  //   close(p[0]);
+  //   write(p[1], " ", 1);
+  //   wait(0);
+  //   read(p[0], buf, sizeof(buf));
+  //   printf("%d: received pong\n",getpid());
+  //   // close(p[1]);
+  // }
+
+
+  //只要保证read可以执行完毕即可，即能读到数据或者写端fd全部被关闭，读到EOF
   if (fork() == 0) {
-    close(p[1]);
-    close(pp[0]);
     read(p[0], buf, sizeof(buf));
     printf("%d: received ping\n",getpid());
-    write(pp[1], " ", 1);
-    close(p[0]);
-    close(pp[1]);
-  }  else {
-    close(p[0]);
-    close(pp[1]);
     write(p[1], " ", 1);
-    read(pp[0], buf, sizeof(buf));
+  }  else {
+    write(p[1], " ", 1);
+    read(p[0], buf, sizeof(buf));
     printf("%d: received pong\n",getpid());
-    close(p[1]);
-    close(pp[0]);
   }
+
   exit(0);
 }
